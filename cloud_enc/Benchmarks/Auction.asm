65df58dcc16fab043cb3523054c43035e68f326c
Rick <rick@sunscreen.tech>
2025-05-30 22:34:31 +0000

    Update auction to use public tape

--- cloud_enc/Benchmarks/Auction.asm
+++ cloud_enc/Benchmarks/Auction.asm
@@ -1,17 +1,23 @@
-econst t0, 0
-move t1, 0
+pubread t0
+secread t1
+move t2, t1
+move t3, 1
 __L1_MAIN__:
-cmpl v1, t1, 32
-move t2, v1
-beq t2, zero, __L2_MAIN__
-secread t2
-ecmpl v1, t0, t2
+cmpl v1, t3, t0
+move t4, v1
+beq t4, zero, __L2_MAIN__
+secread t4
+ecmpl v1, t2, t4
+move t5, v1
+emux t5, t5, t4, t2
+move t2, t5
+ecmpl v1, t5, t4
+move t5, v1
+emux t5, t5, t4, t1
+move t1, t5
+add v1, t3, 1
 move t3, v1
-emux t3, t3, t2, t0
-move t0, t3
-add v1, t1, 1
-move t1, v1
 j __L1_MAIN__
 __L2_MAIN__:
-answer t0
+answer t2
 
