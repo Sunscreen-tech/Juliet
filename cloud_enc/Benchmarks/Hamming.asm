b64a4eb3369b75afde366930fe7cf969c156df15
Rick <rick@sunscreen.tech>
2025-06-02 20:47:00 +0000

    Update Hamming benchmark

--- cloud_enc/Benchmarks/Hamming.asm
+++ cloud_enc/Benchmarks/Hamming.asm
@@ -1,37 +1,34 @@
 econst t0, 0
-econst t1, 0
-econst t2, 0
-econst t3, 0
-econst t4, 0
-econst t5, 0
-econst t6, 0
-move t2, t6
-secread t6
-move t0, t6
-secread t6
-move t1, t6
-exor v1, t0, t1
-move t0, v1
-move t3, t0
-move t0, 0
+pubread t1
+move t2, 0
 __L1_MAIN__:
-move t1, 32
-cmpl v1, t0, t1
-move t1, v1
-beq t1, zero, __L2_MAIN__
-eslr v1, t3, t0
-move t1, v1
-move t4, t1
-econst t1, 1
-eand v1, t4, t1
-move t1, v1
-move t5, t1
-eadd v1, t2, t5
-move t1, v1
-move t2, t1
-add v1, t0, 1
-move t0, v1
+cmpl v1, t2, t1
+move t3, v1
+beq t3, zero, __L2_MAIN__
+secread t3
+secread t4
+exor v1, t3, t4
+move t5, v1
+move t6, 0
+__L3_MAIN__:
+cmpl v1, t6, 8
+move t7, v1
+beq t7, zero, __L4_MAIN__
+eslr v1, t5, t6
+move t7, v1
+econst t8, 1
+eand v1, t7, t8
+move t7, v1
+eadd v1, t0, t7
+move t7, v1
+move t0, t7
+add v1, t6, 1
+move t6, v1
+j __L3_MAIN__
+__L4_MAIN__:
+add v1, t2, 1
+move t2, v1
 j __L1_MAIN__
 __L2_MAIN__:
-answer t2
+answer t0
 
